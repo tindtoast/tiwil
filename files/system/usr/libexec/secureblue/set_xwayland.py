f325911b2ebce373319813787dc6bcb112f34710
tindtoast <>
2026-06-23 00:34:57 +0800

    feat: sync files to secureblue

--- files/system/usr/libexec/secureblue/set_xwayland.py
+++ files/system/usr/libexec/secureblue/set_xwayland.py
@@ -9,27 +9,31 @@
 import os
 import subprocess
 import sys
-from typing import Final
+from typing import TYPE_CHECKING, Final
 
-from utils import (
-    CommandUsageError,
-    Image,
-    ToggleMode,
-    booted_image_ref,
-    parse_basic_toggle_args,
-)
+if TYPE_CHECKING:
+    from files.system.usr.libexec.secureblue import utils
+else:
+    import utils
+
+CommandUsageError: Final = utils.CommandUsageError
+Image: Final = utils.Image
+ToggleMode: Final = utils.ToggleMode
+booted_image_ref: Final = utils.booted_image_ref
+logout: Final = utils.logout
+parse_basic_toggle_args: Final = utils.parse_basic_toggle_args
 
 XWAYLAND_OVERRIDE_FILES: Final[dict[Image, str]] = {
+    Image.TIWIL: "/etc/systemd/user/plasma-kwin_wayland.service.d/override.conf", # custom
     Image.SILVERBLUE: "/etc/systemd/user/org.gnome.Shell@user.service.d/override.conf",
     Image.KINOITE: "/etc/systemd/user/plasma-kwin_wayland.service.d/override.conf",
-    Image.TIWIL: "/etc/systemd/user/plasma-kwin_wayland.service.d/override.conf", # custom
     Image.SERICEA: "/etc/sway/config.d/99-noxwayland.conf",
 }
 
 DE_NAMES: Final[dict[Image, str]] = {
+    Image.TIWIL: "KDE Plasma", # well as of now it is
     Image.SILVERBLUE: "GNOME",
     Image.KINOITE: "KDE Plasma",
-    Image.TIWIL: "KDE Plasma", # well as of now it is
     Image.SERICEA: "Sway",
 }
 
@@ -64,26 +68,30 @@ def run(mode: ToggleMode) -> int:
     image = Image.from_image_ref(booted_image_ref())
     if image not in XWAYLAND_OVERRIDE_FILES:
         print("The booted image does not support toggling Xwayland.")
-        print(image) # just gives me debug info
         return 1
 
     override_file = XWAYLAND_OVERRIDE_FILES[image]
     de_name = DE_NAMES[image]
-    enabled = not os.path.exists(override_file)
+    current_mode_enabled = not os.path.exists(override_file)
+    new_mode = "disabled" if current_mode_enabled else "enabled"
+    logout_prompt = (
+        f"Xwayland for {de_name} has been {new_mode}. "
+        "Would you like to log out now for this to take effect?"
+    )
 
     match mode:
         case ToggleMode.STATUS:
-            print("enabled" if enabled else "disabled")
+            print("enabled" if current_mode_enabled else "disabled")
         case ToggleMode.ON:
-            if enabled:
+            if current_mode_enabled:
                 print(f"Xwayland for {de_name} is already enabled.")
             else:
                 subprocess.run(
                     ["/usr/bin/run0", "/usr/bin/rm", "-f", "--", override_file], check=True
                 )
-                print(f"Xwayland for {de_name} has been enabled. Reboot to take effect.")
+                logout(prompt=logout_prompt)
         case ToggleMode.OFF:
-            if enabled:
+            if current_mode_enabled:
                 subprocess.run(
                     [
                         "/usr/bin/run0",
@@ -95,7 +103,7 @@ def run(mode: ToggleMode) -> int:
                     ],
                     check=True,
                 )
-                print(f"Xwayland for {de_name} has been disabled. Reboot to take effect.")
+                logout(prompt=logout_prompt)
             else:
                 print(f"Xwayland for {de_name} is already disabled.")
 
